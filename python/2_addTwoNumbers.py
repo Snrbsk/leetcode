92758b4e3ae686167bdd2c0d02d92a5ae8460146
Soner BAŞAK <sonerbasaak@gmail.com>
2024-10-06 11:28:07 +0300

    addTwoNumbers

---
+++ python/2_addTwoNumbers.py
@@ -0,0 +1,43 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode()
+        current = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            
+            total = val1 + val2 + carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
+            
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+                
+        return dummy.next
+
+def print_linked_list(node):
+    while node:
+        print(node.val, end=" -> " if node.next else "\n")
+        node = node.next
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+solution = Solution()
+result = solution.addTwoNumbers(l1, l2)
+print_linked_list(result)
